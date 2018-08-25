6d2a67872d685d5423099a861450a4bfa93c14e2
LcNdinda <lcndinda@gmail.com>
2018-08-25 14:55:53 +0300

    Add effect to image dice

--- js/script.js
+++ js/script.js
@@ -1,34 +1,12 @@
-// Business Logic
-function Dice(randomNumber){
-  this.randomNumber = randomNumber;
-  this.scores = []
-}
-
-function Score(scores){
- this.scores = scores;
-};
-
-Dice.prototype.diceRoll = function(){
-  total = 0;
-  this.scores.forEach(function(score){
-    return total + score;
-  });
-};
-
-Dice.prototype.totalScore  = function(){
-  return this.randomNumber + this.scores;
-}
-
- var randomize = function(){
-   var randomNumber = Math.floor((Math.random() * 6) + 1);
-   return randomNumber;
- }
-
-
 // USER LOGIC
 $(document).ready(function(){
-  $("button#player1").click(function(){
-    var player1 = randomize();
-    $("#player1Results").append("<p>" + player1 +"</p>")
+  $("#img1").click(){
+    $("img1").hide();
+    $("img2").show();
+  }
+
+  $("#img2").click(function(){
+    $("#img1").show();
+    $("#img2").hide();
   })
 })

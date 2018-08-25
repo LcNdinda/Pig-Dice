00dff8164db21f7d36cddfa8518bd39eef696375
LcNdinda <lcndinda@gmail.com>
2018-08-25 15:28:56 +0300

    Add a function to randomize numbers

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
+
+var randomize = function(){
+  var randomNumber = Math.floor((Math.random() * 6) + 1);
+   return randomNumber;
+}
+
 // USER LOGIC
 $(document).ready(function(){
-  $("#img1").click(){
-    $("img1").hide();
-    $("img2").show();
-  }
+  $("#img1").click(function(){
+    $("#img1").hide();
+    $("#img2").show();
+  });
 
   $("#img2").click(function(){
     $("#img1").show();
     $("#img2").hide();
+  });
+  // Player 1
+  $("#btn-player1").click(function(){
+    var player1 = randomize();
+    $(".player1Result").text(player1);
+  });
+
+  // Player 2
+
+  $("#btn-player2").click(function(){
+    var player2 = randomize();
+      $(".player2Result").text(player2);
   })
-})
+
+});

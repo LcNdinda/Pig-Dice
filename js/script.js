b265a0bbc99c38f2686a8be48f17e3716d84f3ea
LcNdinda <lcndinda@gmail.com>
2018-08-25 21:51:22 +0300

    Add a function to calculate score

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
 // Business LOGIC
-function Dice(roll, score, player){
+function Dice(roll, score, player, turn){
   this.roll = roll;
   this.score = score;
-
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.player = player;
 };
 
-// DiceRoll.prototype.count = function(){
-//   if (this.roll = 1) {
-//     this.score =0;
-//     alert("Sorry Player1, you rolled a 1. Your turn is over.")
-//   }
-//   else {
-//     this.score +=this.roll;
-//   }
-// };
-
-var calcScore = function(number){
-  var tempScore = 0;
-
-  if (number ===1) {
-    return "Sorry You rolled a 1. Score is zero"
+Dice.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  alert("Sorry " + this.player + ", you rolled a 1! Your turn is over!")
+  // this.changeturn();
+  } else {
+  this.tempscore += this.roll;
   }
-  else {
-    return tempScore += number;
+}
+
+Dice.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert(this.player + ", your turn is over, pass the mouse!");
+}
+
+Dice.prototype.winnerCheck = function () {
+  if (this.totalscore >= 100) {
+    alert(this.player+ " You are the winner!");
   }
+}
+
+
+var calcScore = function(numbers){
+  var tempscore = 0
+
+        if (numbers === 1) {
+      return "Sorry You rolled a 1. Score is zero"
+    }
+
+    else {
+
+      return  tempscore += numbers;
+
+    }
 
 }
 
@@ -51,15 +70,16 @@ $(document).ready(function(){
   });
 
   $(".play").click(function(){
-    $(".row").show();
+    $(".row").fadeIn("slow");
+    $("#mainImg").hide();
 
   })
   // Player 1
   $("#btn-player1").click(function(){
     var player1 = randomize();
     $(".player1Result").text(player1);
-    var score = calcScore(player1);
-    $(".player1Score").text(score);
+    var score1 = calcScore(player1);
+    $(".player1Score").text(score1);
   });
 
   // Player 2
@@ -67,6 +87,9 @@ $(document).ready(function(){
   $("#btn-player2").click(function(){
     var player2 = randomize();
       $(".player2Result").text(player2);
+      var score2 = calcScore(player2);
+      $(".player2Score").text(score2);
+
   })
 
   $("#blue").click(function(){
